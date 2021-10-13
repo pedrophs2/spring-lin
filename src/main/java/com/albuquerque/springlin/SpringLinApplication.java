cb5f187a899c41c3a422887310cd173519a7c445
pedrophs2 <38799159+pedrophs2@users.noreply.github.com>
2021-10-13 06:59:04 -0300

    Initial commit

---
+++ src/main/java/com/albuquerque/springlin/SpringLinApplication.java
@@ -0,0 +1,13 @@
+package com.albuquerque.springlin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/albuquerque/springlin/SpringLinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albuquerque.springlin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
